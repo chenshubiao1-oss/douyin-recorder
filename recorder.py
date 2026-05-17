aa8b3062242b605a5fc1a7768aa0985713a8430c
tangwei880620-rgb <tangwei880620@gmail.com>
2026-05-17 16:15:36 +0700

    revert ffmpeg loglevel warning, remove log files

--- recorder.py
+++ recorder.py
@@ -208,13 +208,12 @@ def start_recording(url, quality, room_id, anchor_name=""):
         "Connection: keep-alive\r\n"
         + cookie_hdr,
     ]
-    logfile = os.path.join(os.path.dirname(outfile), os.path.basename(outfile) + ".ffmpeg.log")
-    proc = subprocess.Popen([FFMPEG, "-y", "-loglevel", "info"] + ff_headers + ["-i", url, "-c", "copy",
+    proc = subprocess.Popen([FFMPEG, "-y", "-loglevel", "warning"] + ff_headers + ["-i", url, "-c", "copy",
                              "-movflags", "+faststart+frag_keyframe+empty_moov", "-f", "mp4", outfile],
-                            stdout=subprocess.DEVNULL, stderr=open(logfile, 'w'))
-    audio_proc = subprocess.Popen([FFMPEG, "-y", "-loglevel", "info"] + ff_headers + ["-i", url, "-vn",
+                            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    audio_proc = subprocess.Popen([FFMPEG, "-y", "-loglevel", "warning"] + ff_headers + ["-i", url, "-vn",
                                     "-acodec", "pcm_s16le", "-ar", "16000", "-ac", "1", audiofile],
-                                   stdout=subprocess.DEVNULL, stderr=open(logfile + ".audio", 'w'))
+                                   stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     return proc, outfile, audio_proc, audiofile
 
 
